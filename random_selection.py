9452c21916d1a68bfdddca6231e5c3da2c5cb965
Johan Zicola <johan.zicola@gmail.com>
2018-10-23 23:28:17 +0200

    Add functions to search for mp3 files and make a random sublist from it. Works.

--- random_selection.py
+++ random_selection.py
@@ -4,6 +4,10 @@ from Tkinter import *
 from tkSimpleDialog import askstring
 import Tkinter, tkMessageBox, Tkconstants, tkFileDialog, os
 import Tkinter as tk
+import fnmatch # to search file recursively
+import random # Select random songs from a list
+
+# This script runs on Python2.7
 
 
 ## Layout pseudocode
@@ -13,9 +17,9 @@ import Tkinter as tk
 # Function requiring directory to user
 # Variable 'dir' created to personalize the GUI text
 def get_dirname(dir):
-    # withdraw allows to remove the default root window displayed by Tk
+    # withdraw removes the default root window displayed by Tk
     Tk().withdraw()
-    dirname = tkFileDialog.askdirectory(initialdir=os.getcwd(),title='Please select '+dir)
+    dirname = tkFileDialog.askdirectory(initialdir=os.getcwd(), title='Please select '+dir)
     if len(dirname) > 0:
         print ("You chose %s" % dirname)
         return dirname
@@ -25,34 +29,56 @@ def get_dirname(dir):
         return dirname 
 
 
-#dir = "directory containing music"
-#dir_input = get_dirname(dir)
+dir_input = get_dirname("directory containing music")
 
-#dir = "destination directory"
-#dir_output = get_dirname(dir)
+#dir_output = get_dirname("destination directory")
 
-
-while True:
-
-    try:
-        # Ask for the number of songs requested
-        root = tk.Tk()
-        # show askstring dialog without the Tkinter window
-        root.withdraw()
-        # Get value and check if integer
-        nb_song = int(askstring("Number of songs", ""))
-    except ValueError:
-        tkMessageBox.showerror("Error message", "Value provided is not an integer")
-    else:
-        # Value given is an integer
-        break
-   
 
 # Check if number given is an integer
 
+def get_nb_song():
+    while True:
+        try:
+            # Ask for the number of songs requested
+            root = tk.Tk()
+            # show askstring dialog without the Tkinter window
+            root.withdraw()
+            # Get value and check if integer
+            nb_song = int(askstring("Number of songs", ""))
+        except ValueError:
+            tkMessageBox.showerror("Error message", "Value provided is not an integer")
+        else:
+            # Value given is an integer
+            break
+    return nb_song
+
+nb_song = get_nb_song()
+
 # Find mp3 in directory and select a random
 
+def find_mp3(dir_input):
+    matches = []
+    for root, dirnames, filenames in os.walk(dir_input):
+        for filename in fnmatch.filter(filenames, '*.mp3'):
+            matches.append(os.path.join(root, filename))
+    if len(matches) == 0:
+        tkMessageBox.showerror("Error message", "No mp3 files were found in "+dir_input) 
+    else:
+        return matches
+
+list_song = find_mp3(dir_input)
+
+
 # Get a random selection of x files within the search results
+def select_random(list_song, nb_song):
+    if len(list_song) < nb_song:
+        nb_song = len(list_song)
+    sub_list = random.sample(list_song, nb_song)
+    return sub_list
+
+sub_list = select_random(list_song, nb_song)
+print sub_list
+
 
 # Copy these files into the output directory
 
